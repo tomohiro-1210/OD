57b4c918c80fd81d032a51041e8a04250f614d95
tomohiro_0225 <express22212270@gmail.com>
2024-06-17 16:59:19 +0900

    事業内容ページ、会社概要ページコーディング完了。 下層ページすべての見直し、微修正かけた。

--- cording_template/assets/js/common.js
+++ cording_template/assets/js/common.js
@@ -3,3 +3,23 @@ $(".l-header__hamburger").click(function () {
     $(this).toggleClass('open');
     $(".l-header-sp-menu").toggleClass('open');
 });
+
+// フェードインのアニメーション
+$(window).on("scroll", function() {
+    //フェードイン
+    $('[data-fadeIn]').each(function(index, el) {
+        if( $(window).scrollTop() > ( $(el).offset().top - 100 ) ){
+            $(el).addClass('is-over');
+        }
+    });
+    $('[data-leftIn]').each(function(index, el) {
+        if( $(window).scrollTop() > ( $(el).offset().top - 100 ) ){
+            $(el).addClass('is-over');
+        }
+    });
+    $('[data-rightIn]').each(function(index, el) {
+        if( $(window).scrollTop() > ( $(el).offset().top - 100 ) ){
+            $(el).addClass('is-over');
+        }
+    });
+  });
