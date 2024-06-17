7031227907c4b51ba11a95140c0378a9a9e954a1
tomohiro_0225 <express22212270@gmail.com>
2024-06-17 11:15:24 +0900

    特定商取引法 プライバシーポリシー お問い合わせフォーム お知らせ詳細 お知らせ一覧 募集要項ページコーディング完了

--- cording_template/assets/js/common.js
+++ cording_template/assets/js/common.js
@@ -1,5 +1,5 @@
 // headerのハンバーガーメニュー
 $(".l-header__hamburger").click(function () {
     $(this).toggleClass('open');
-    $(".sp-menu").toggleClass('open');
+    $(".l-header-sp-menu").toggleClass('open');
 });
