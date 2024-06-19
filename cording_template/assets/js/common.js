58929e9b618d2592916ec3721ca77f5ebd2e6dde
tomohiro_0225 <express22212270@gmail.com>
2024-06-19 11:17:13 +0900

    サイトのアニメーション実装や微修正終わり

--- cording_template/assets/js/common.js
+++ cording_template/assets/js/common.js
@@ -15,6 +15,26 @@ $(function() {
     $('.l-fv__right').addClass('show');
 });
 
+// スクロールしたときのフェードイン
+$(window).on("scroll", function() {
+    //フェードイン
+    $('[data-fadeIn]').each(function(index, el) {
+        if( $(window).scrollTop() > ( $(el).offset().top - $(window).height()) ){
+            $(el).addClass('show');
+        }
+    });
+    $('[data-leftIn]').each(function(index, el) {
+        if( $(window).scrollTop() > ( $(el).offset().top - $(window).height()) ){
+            $(el).addClass('show');
+        }
+    });
+    $('[data-rightIn]').each(function(index, el) {
+        if( $(window).scrollTop() > ( $(el).offset().top - $(window).height()) ){
+            $(el).addClass('show');
+        }
+    });
+});
+
 
 //   fvのスライダー
 $(function () {
@@ -36,3 +56,23 @@ $(function () {
         arrows: false,
     });
   });
+
+// ロゴの切り替え
+$(document).ready(function() {
+    const whiteLogo = $('.white-logo');
+    const blackLogo = $('.black-logo');
+    var kv = $('.l-kv'); // 対象の要素
+    var kvOffsetTop = kv.outerHeight();
+
+    $(window).on('scroll', function() {
+        var scrollTop = $(this).scrollTop();
+
+        if (scrollTop > kvOffsetTop) {
+            whiteLogo.addClass('hide');
+            blackLogo.addClass('show');
+        } else {
+            whiteLogo.removeClass('hide');
+            blackLogo.removeClass('show');
+        }
+    });
+});
